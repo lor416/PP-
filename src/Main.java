12e21817ace5c52892112168868fef33b79de6b3
Karpovich Yuliya <339008080.yuluya@gmail.com>
2025-09-22 21:59:41 +0300

    input of user data

--- src/Main.java
+++ src/Main.java
@@ -7,20 +7,29 @@ public class Main {
         InputStreamReader isr = new InputStreamReader(System.in);
         BufferedReader br = new BufferedReader(isr);
         try{
+            System.out.println("Введите исходную строку");
+            String allLine = br.readLine();
+
+            System.out.println("Введите вставляемое слово");
             String line = br.readLine();
-            System.out.println(line);
+            //System.out.println(line);
+
+            System.out.println("Введите количество букв");
+            int num = Integer.parseInt(br.readLine());
+
 
 
             StringBuilder sb = new StringBuilder();
-            StringTokenizer st = new StringTokenizer("Строка, которую мы хотим разобрать на слова", " \t\n\r");
+            StringTokenizer st = new StringTokenizer(allLine, " \t\n\r");
             while(st.hasMoreTokens()){
                 String str = st.nextToken();
-                if(str.length() == 2)
+                if(str.length() == num)
                     sb.append(line);
+
                 else {
                     sb.append(str);
                 }
-                sb.append(" ");
+                sb.append(' ');
             }
             System.out.println(sb.toString());
 
