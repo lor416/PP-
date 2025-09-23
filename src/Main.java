98e26d66ccb41e5cbb40b414b1212753fbc69316
Karpovich Yuliya <339008080.yuluya@gmail.com>
2025-09-23 12:20:06 +0300

    Punctuation length of word error

--- src/Main.java
+++ src/Main.java
@@ -7,27 +7,49 @@ public class Main {
         InputStreamReader isr = new InputStreamReader(System.in);
         BufferedReader br = new BufferedReader(isr);
         try{
+
+            System.out.println("Введите исходный текст (для завершения введите пустую строку):");
+            StringBuilder inputText = new StringBuilder();
+            String lineInput;
+            while (!(lineInput = br.readLine()).isEmpty()) {
+                inputText.append(lineInput).append("\n");
+            }
+
+            String allLine = inputText.toString().trim();
+
+            /*
             System.out.println("Введите исходную строку");
-            String allLine = br.readLine();
+            //String allLine = br.readLine();
+            String allLine = "Строка, да, которую мы меняем: и! один два; три) ()  ";
+            System.out.println(allLine);*/
 
             System.out.println("Введите вставляемое слово");
             String line = br.readLine();
-            //System.out.println(line);
 
             System.out.println("Введите количество букв");
             int num = Integer.parseInt(br.readLine());
 
-
-
-            StringBuilder sb = new StringBuilder();
-            StringTokenizer st = new StringTokenizer(allLine, " \t\n\r");
+            StringBuffer sb = new StringBuffer();
+            StringTokenizer st = new StringTokenizer(allLine, " \t\r");
             while(st.hasMoreTokens()){
                 String str = st.nextToken();
-                if(str.length() == num)
-                    sb.append(line);
 
-                else {
-                    sb.append(str);
+                char lastChar = str.charAt(str.length() - 1);
+                int length = str.length();
+                boolean hasPunctuation = (lastChar == ',' || lastChar == '.' ||
+                        lastChar == ':' || lastChar == ';'|| lastChar == '!' ||
+                        lastChar == ')' || lastChar == '(');
+
+                if(hasPunctuation)
+                    if(length-1 == num)
+                        sb.append(line);
+                    else
+                        sb.append(str);
+                else{
+                    if(length == num)
+                        sb.append(line);
+                    else
+                        sb.append(str);
                 }
                 sb.append(' ');
             }
@@ -37,6 +59,5 @@ public class Main {
         catch (IOException e){
             System.out.println("Ошибка чтения с клавиатуры");
         }
-
     }
 }
